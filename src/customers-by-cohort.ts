fa80fd69fe7bb7462eca5d85ba1503fe5a0234bb
Paul Shorey <web@artspaces.net>
2023-01-02 23:28:48 -0600

    solved 1-3, new fork, to merge into own repo main

--- src/customers-by-cohort.ts
+++ src/customers-by-cohort.ts
@@ -1,5 +1,70 @@
 import { invoiceData } from './invoice-data';
 
+type customerFromDB = number;
+type customerIdAsKey = string;
+type yearMo = string;
+type customerIds = customerFromDB[];
+
+const cohortsFinal: Record<yearMo, customerIds> = {};
+const cohortCustomers: Record<yearMo, Record<customerIdAsKey, true>> = {};
+const customerCohort: Record<customerIdAsKey, yearMo> = {};
+let yearMin = 0;
+let yearMax = 0;
+let monthMin = 0;
+let monthMax = 0;
+
+// 1. Aggregate data
+for (const { customer: customer, timestamp: timestamp } of invoiceData) {
+  // get year of timestamp
+  const year = new Date(timestamp).getFullYear();
+  // get month of timestamp
+  const month = new Date(timestamp).getMonth() + 1;
+  // build YYYY-MM string from timestamp
+  const yearMo = year.toString() + '-' + month.toString().padStart(2, '0');
+
+  // get or set customer cohort
+  if (customerCohort[customer]) {
+    continue;
+  }
+  customerCohort[customer] = yearMo;
+
+  // save customer to cohort
+  if (!cohortCustomers[yearMo]) {
+    cohortCustomers[yearMo] = {};
+  }
+  cohortCustomers[yearMo][customer] = true;
+
+  // range of cohorts and months
+  if (yearMin === 0 || year < yearMin) {
+    yearMin = year;
+  }
+  if (year > yearMax) {
+    yearMax = year;
+  }
+  if (monthMin === 0 || month < monthMin) {
+    monthMin = month;
+  }
+  if (month > monthMax) {
+    monthMax = month;
+  }
+}
+
+// 2. Sort
+// loop through each cohort
+for (let year = yearMin; year <= yearMax; year++) {
+  for (let month = monthMin; month <= monthMax; month++) {
+    const yearMo = year.toString() + '-' + month.toString().padStart(2, '0');
+    const customers = cohortCustomers[yearMo]
+      ? Object.keys(cohortCustomers[yearMo]).map((c) => parseInt(c))
+      : [];
+    //.sort(); // optional - sort if invoice data is not already sorted
+    cohortsFinal[yearMo] = customers;
+  }
+}
+
+// 3. Print customers by cohort
+console.log(cohortsFinal);
+
 // Write a script to group each customer into their "cohort"
 // A customer's cohort is the month they paid their first non-zero invoice.
 
--- src/top-ten-customers.ts
+++ src/top-ten-customers.ts
@@ -1,5 +1,29 @@
 import { invoiceData } from './invoice-data';
 
+type customeridAsKey = string;
+type amount = number;
+type customerTotalAmounts = Record<customeridAsKey, amount>;
+type sortedCustomers = [customeridAsKey, amount][];
+
+// 1. Aggregate data
+const customerTotalAmounts: customerTotalAmounts = {};
+for (const { customer: customer, amount: amount } of invoiceData) {
+  // initiate new customer
+  if (customerTotalAmounts[customer] === undefined) {
+    customerTotalAmounts[customer] = 0;
+  }
+  // add new amount to customer's total
+  customerTotalAmounts[customer] += amount;
+}
+
+// 2. Sort customers by amount
+const sortedCustomers = Object.entries(customerTotalAmounts).sort(
+  ([, amountA], [, amountB]) => amountB - amountA
+);
+
+// 3. Print top 10 customers
+console.log(sortedCustomers.slice(0, 10));
+
 // Write a script to find the ten customers who have spent the most overall
 // The output should be sorted by the amount spent and include the customer ID.
 // For example:
